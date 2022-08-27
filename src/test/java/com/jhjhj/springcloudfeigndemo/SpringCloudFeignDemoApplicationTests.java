20cc51fb219901c8d43b066b09a3ed8432c925a5
jeonghyunjeong <dev.jhjhj@gmail.com>
2022-08-27 12:44:43 +0900

    Initial commit

---
+++ src/test/java/com/jhjhj/springcloudfeigndemo/SpringCloudFeignDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhjhj.springcloudfeigndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFeignDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
